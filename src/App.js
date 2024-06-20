f6fa9df0e83885d68c0de211bcdddc4ee5f8e9da
hg_yellow <jang010505@gmail.com>
2024-06-21 07:14:14 +0900

    feat: #3 Add connect info api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/SignUpPage';
 import SignUpPage from './pages/SignUpPage';
 import SingInPage from './pages/SignInPage';
+import UpdateInfo from './pages/UpdateInfo';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-            <Route path="/" element = {<MainPage/>}></Route>
-            <Route path="/sign-up" element = {<SignUpPage/>}></Route>
-            <Route path="/sign-in" element = {<SingInPage/>}></Route>
+          <Route path="/" element={<MainPage />}></Route>
+          <Route path="/sign-up" element={<SignUpPage />}></Route>
+          <Route path="/sign-in" element={<SingInPage />}></Route>
+          <Route path='/info/update/:infoId' element={<UpdateInfo />} />
         </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Description.js
+++ src/components/Description.js
@@ -1,33 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
+import { fetchPersonInfo } from '../api/FetchInfo';
+import Info from './Info';
 
 const Description = () => {
+  const [infos, setInfos] = useState([]);
+
+  useEffect(() => {
+    const data = fetchPersonInfo(1);
+    data.then(result => setInfos(result))
+  }, []);
   return (
-    <Paper style={{ padding: 16}}>
+    <Paper style={{ padding: 16 }}>
       <Typography variant="h5" component="h2">
         설명
       </Typography>
       <Typography component="p">
         <ul>
-          <li>ㅋㅋ</li>
-          <li>ㅋㅋ</li>
-          <li>ㅋㅋ</li>
-          <li>ㅋㅋ</li>
-          <li>ㅋㅋ</li>
-          <li>ㅋㅋ</li>
-          <li>ㅋㅋ</li>
-          <li>ㅋㅋ</li>
-          <li>ㅋㅋ</li>
-          <li>ㅋㅋ</li>
-          <li>ㅋㅋ</li>
-          <li>ㅋㅋ</li>
-          <li>ㅋㅋ</li>
-          <li>ㅋㅋ</li>
-          <li>ㅋㅋ</li>
-          <li>ㅋㅋ</li>
-          <li>ㅋㅋ</li>
-
+          {infos && infos.map((info, idx) => {
+            return <Info key={idx} data={info}></Info>
+          })}
         </ul>
         여기에 설명 내용을 작성하세요. 이 부분은 문서의 자세한 정보를 포함합니다.
       </Typography>
