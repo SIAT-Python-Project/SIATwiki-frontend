aa7ab9230fba43f11231dedfa4a441287940420c
wisdom <52325199+Wisdom-Kim@users.noreply.github.com>
2024-06-21 17:19:49 +0900

    Feat: 자동완성 기능
    말그대로 자동완성만 했습니다
    검색으로 넘어가는거 구현해야해요ㅠㅠ

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import SignUpPage from './pages/SignUpPage';
 import SingInPage from './pages/SignInPage';
 import UpdateInfo from './pages/UpdateInfo';
 import PersonPage from './pages/PersonPage';
+import CreatePersonPage from './pages/CreatePersonPage';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
+            <Route path="/person/creation" element = {<CreatePersonPage/>}></Route>
             <Route path="/person/:id" element = {<PersonPage/>}></Route>
             <Route path="/api/sign-up" element = {<SignUpPage/>}></Route>
             <Route path="/api/login" element = {<SingInPage/>}></Route>
           <Route path='/info/update/:infoId' element={<UpdateInfo />} />
+          
         </Routes>
       </BrowserRouter>
     </div>
