5c62a794c14030772e4e2af14e0f413b5443ec1f
hg_yellow <jang010505@gmail.com>
2024-06-21 11:23:46 +0900

    feat: #9 Add markdown style

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,8 +1,12 @@
 import React from "react";
-import ReactMarkdown from "react-markdown";
-import remarkGfm from 'remark-gfm'
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
+import ReactMarkdown from "react-markdown";
+import remarkGfm from 'remark-gfm'
+import '../css/markdown.css'
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/a11y-dark.css";
+import rehypeRaw from "rehype-raw";
 
 const InfoHeader = styled.div`
     display: flex;
@@ -27,7 +31,7 @@ export default function Info({ data }) {
                 <MyBtn onClick={() => navigate(`/info/update/${data.id}`)}> [ 수정 ] </MyBtn>
             </InfoHeader>
             <hr />
-            <ReactMarkdown remarkPlugins={[remarkGfm]}>
+            <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeHighlight, rehypeRaw]}>
                 {data.content}
             </ReactMarkdown>
         </>
